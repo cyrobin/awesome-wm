19730a1f0141a87437fbfd6cd7691e3fef8a1ab8
Cyril Robin <cyril.robin@laas.fr>
2014-12-01 10:29:46 +0100

    Handle youtube fullscreen

--- rc/rules.lua
+++ rc/rules.lua
@@ -4,11 +4,16 @@ awful.rules.rules = {
    --{{{ All clients will match this rule.
    { rule = { },
      properties = { border_width = beautiful.border_width,
-		    border_color = beautiful.border_normal,
-		    focus = true,
-		    maximized_vertical   = false,
-		    maximized_horizontal = false,
-                    keys = config.keys.client,
-		    buttons = config.mouse.client }},
+        border_color = beautiful.border_normal,
+        focus = true,
+        maximized_vertical   = false,
+        maximized_horizontal = false,
+        keys = config.keys.client,
+        buttons = config.mouse.client }},
    --}}}
+   -- YouTube fullscreen mode in Firefox (1st) and Chromium (2nd)
+    { rule = { instance = "plugin-container" },
+      properties = {floating = true }},
+    { rule = { instance = "exe" },
+      properties = {floating = true }},
 }
